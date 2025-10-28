883edef80e524802863af1e70a51f06a5fb834a7
Abin-tek <abinathmouniane@gmail.com>
2025-10-28 13:15:47 +0530

    fix: mongo uri fixed

--- db/main.js
+++ db/main.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 // Funtion to connect database
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI + "task-manager");
+    await mongoose.connect(process.env.MONGO_URI + "/task-manager");
     console.log("Connected to database");
   } catch (error) {
     console.error("Error connecting to db: ", error);
